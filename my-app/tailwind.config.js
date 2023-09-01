878826eed15e815934d3669bbc277ee81be46cee
AbhigyanBafna <101444239+AbhigyanBafna@users.noreply.github.com>
2023-09-01 11:11:54 +0530

    App Simplify

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'cyberpunk': ['Cyberpunk', 'sans-serif'], // Add your font family here
+      },
+    },
   },
   plugins: [],
 }
