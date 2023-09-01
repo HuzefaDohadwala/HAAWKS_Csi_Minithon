868470a52b2dcdeebf579b803d2249d454aaf5f3
Adityaapandkar <adityaapandkar11@gmail.com>
2023-09-01 10:14:57 +0530

    Data for charts

---
+++ my-app/src/Components/DataTrial.jsx
@@ -0,0 +1,32 @@
+export const UserData = [
+  {
+    id: 1,
+    year: 2019,
+    moneyGain: 3000,
+    moneyLost: 2000,
+  },
+  {
+    id: 2,
+    year: 2020,
+    moneyGain: 3000,
+    moneyLost: 2000,
+  },
+  {
+    id: 3,
+    year: 2021,
+    moneyGain: 3000,
+    moneyLost: 2000,
+  },
+  {
+    id: 4,
+    year: 2022,
+    moneyGain: 3000,
+    moneyLost: 2000,
+  },
+  {
+    id: 5,
+    year: 2023,
+    moneyGain: 3000,
+    moneyLost: 2000,
+  },
+];
