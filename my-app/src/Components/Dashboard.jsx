d8721640dab203fbb642dc285b8c20de1fde668f
Huzefa Dohadwala <dohadwalahuzefa@gmail.com>
2023-09-01 09:32:58 +0530

    dash-profile

---
+++ my-app/src/Components/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Dashboard.css'
+
+
+const Dashboard = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default Dashboard
---
+++ my-app/src/Components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Profile.css'
+
+const Profile = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Profile
