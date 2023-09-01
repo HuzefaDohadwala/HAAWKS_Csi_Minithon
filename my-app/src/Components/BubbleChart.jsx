199730883289e24ac4ca59bb5a0d59042594c798
Huzefa Dohadwala <dohadwalahuzefa@gmail.com>
2023-09-01 12:25:02 +0530

    graph 3

--- my-app/src/Components/BubbleChart.jsx
+++ my-app/src/Components/BubbleChart.jsx
@@ -1,51 +1,84 @@
 import React, { useState } from 'react';
 import ReactApexChart from 'react-apexcharts';
 
-const generateData = (baseval, count, yrange) => {
-  let i = 0;
-  const series = [];
-  while (i < count) {
-    const x = Math.floor(Math.random() * (750 - 1 + 1)) + 1;
-    const y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
-    const z = Math.floor(Math.random() * (75 - 15 + 1)) + 15;
-
-    series.push([x, y, z]);
-    baseval += 86400000;
-    i++;
-  }
-  return series;
-};
-
 const BubbleChart = () => {
   const [series] = useState([
     {
-      name: 'Bubble1',
-      data: generateData(new Date('11 Feb 2017 GMT').getTime(), 20, {
-        min: 10,
-        max: 60,
-      }),
+      name: 'Programming',
+      data: [[80, 90, 50]]
     },
     {
-      name: 'Bubble2',
-      data: generateData(new Date('11 Feb 2017 GMT').getTime(), 20, {
-        min: 10,
-        max: 60,
-      }),
+      name: 'Communication',
+      data: [[60, 65, 70]]
     },
     {
-      name: 'Bubble3',
-      data: generateData(new Date('11 Feb 2017 GMT').getTime(), 20, {
-        min: 10,
-        max: 60,
-      }),
+      name: 'Project Management',
+      data: [[85, 75, 60]]
     },
     {
-      name: 'Bubble4',
-      data: generateData(new Date('11 Feb 2017 GMT').getTime(), 20, {
-        min: 10,
-        max: 60,
-      }),
+      name: 'Design',
+      data: [[55, 95, 40]]
     },
+    {
+      name: 'Sports',
+      data: [[20, 80, 30]]
+    },
+    {
+      name: 'Singing',
+      data: [[10, 95, 50]]
+    },
+    {
+      name: 'Public Speaking',
+      data: [[70, 60, 80]]
+    },
+    {
+      name: 'Data Analysis',
+      data: [[90, 70, 85]]
+    },
+    {
+      name: 'Cooking',
+      data: [[5, 70, 20]]
+    },
+    {
+      name: 'Marketing',
+      data: [[75, 65, 45]]
+    },
+    {
+      name: 'Teaching',
+      data: [[65, 85, 60]]
+    },
+    {
+      name: 'Writing',
+      data: [[60, 90, 70]]
+    },
+    {
+      name: 'Networking',
+      data: [[55, 50, 65]]
+    },
+    {
+      name: 'Research',
+      data: [[80, 70, 90]]
+    },
+    {
+      name: 'Carpentry',
+      data: [[25, 40, 35]]
+    },
+    {
+      name: 'Photography',
+      data: [[35, 85, 50]]
+    },
+    {
+      name: 'Videography',
+      data: [[40, 80, 45]]
+    },
+    {
+      name: 'Gaming',
+      data: [[5, 75, 20]]
+    },
+    {
+      name: 'Drawing',
+      data: [[10, 90, 25]]
+    }
   ]);
 
   const options = {
@@ -60,15 +93,27 @@ const BubbleChart = () => {
       opacity: 0.8,
     },
     title: {
-      text: 'Simple Bubble Chart',
+      text: 'Skill Proficiency vs. Career Relevance vs. Personal Interest',
     },
     xaxis: {
       tickAmount: 12,
-      type: 'category',
+      type: 'numeric',
+      title: {
+        text: 'Relevance to Career'
+      }
     },
     yaxis: {
-      max: 70,
+      max: 100,
+      title: {
+        text: 'Personal Interest'
+      }
     },
+    z: {
+      range: [0, 100],
+      title: {
+        text: 'Proficiency'
+      }
+    }
   };
 
   return (
